1f988b33562b9f041a9eca64bbc40cfa9bdd9adb
simon <1qazwersdf@gmail.com>
2024-11-21 15:30:24 +0800

    Impements ToDo List

--- main.py
+++ main.py
@@ -1,19 +1,87 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends, HTTPException
 from pydantic import BaseModel
+from sqlalchemy import create_engine, Column, Integer, String, Boolean
+from sqlalchemy.ext.dec.arative import declarative_base
+from sqlalchemy.orm import sessionmaker, Session
+
 
 app = FastAPI()
 
-class Item(BaseModel):
-    name : str
-    price : float
-    description : str = None
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+DATABASE_URL = "sqlite:///./todos.db"
+Base = declarative_base()
+engine = create_engine(DATABASE_URL, connect_args={"check_same_thread" : False})
+SessionLocal = sessionmaker(autocommit = False, autoflush = False, bind = engine)
 
-@app.post("/items/")
-def create_item(item:Item):
-    print(f"Received item:{item}")
-    return {"message": "Item received"}
+# Difine Model
+class Todo(Base):
+    __tablename__ = "todos"
+    id=Column(Integer, primary_key=True, index=True)
+    title = Column(String, nullabe = False)
+    description = Column(String, nullabe = True)
+    completed = Column(Boolean, default=False)
 
+# Initialize Database's Table
+Base.metadata.create_all(bind=engine)
+
+#Pydantic
+class TodoBase(BaseModel):
+    title : str
+    description : str | None=None
+    completed : bool = False
+
+class TodoCreate(TodoBase):
+    pass
+
+class TodoResponse(TodoBase):
+    id: int
+
+    class Config:
+        orm_mode= True
+
+# Database Injection
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+@app.post("/todos", response_model=TodoResponse)
+def created_todo(todo: TodoCreate, db: Session = Depends(get_db())):
+    db_todo = Todo(**todo.dict())
+    db.add(db_todo)
+    db.commit()
+    db.refresh(db_todo)
+    return db_todo
+
+@app.get("/todos", response_model= list[TodoResponse])
+def read_todos(db:Session = Depends(get_db())):
+    return db.query(Todo).all()
+
+@app.get("/todo/{todo_id}]", response_model = TodoResponse)
+def read_todo(todo_id: int , db:Session = Depends(get_db())):
+    db_todo = db.query(Todo).filter(Todo.id == todo_id).first()
+    if not db_todo:
+        raise HTTPException(status_code=404, details="Todo not found")
+    return db_todo 
+
+@app.put("/todo/{todo_id}", response_model=TodoResponse)
+def updata_todo(todo_id: int, todo: TodoCreate, db: Session = Depends(get_db())):
+    db_todo = db.query(Todo).filter(Todo.id == todo_id).first()
+    if not db_todo:
+        raise HTTPException(status_code=404, details="Todo not found")
+    for key, value in todo.dict().items():
+        setattr(db_todo, key, value)
+    db.commit()
+    db.refresh(db_todo)
+    return db_todo
+
+@app.delete("/todo/{todo_id}")
+def delete_todo (todo_id: int, todo: TodoCreate, db: Session = Depends(get_db())):
+    db_todo = db.query(Todo).filter(Todo.id == todo_id).first()
+    if not db_todo:
+        raise HTTPException(status_code=404, details="Todo not found")
+    db.delete(db_todo)
+    db.commit()
+    return {"delete": "Todo deleted successfully"}
