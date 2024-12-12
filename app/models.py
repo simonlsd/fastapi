cb5f3ee317baadee8f8595269d6b36dfa29a9617
simonlsd <1qazwersdf@gmail.com>
2024-12-12 15:00:36 +0800

    Split components

---
+++ app/models.py
@@ -0,0 +1,12 @@
+from sqlalchemy import create_engine, Column, Integer, String, Boolean, Date
+from .models import Base
+
+
+# Define Model
+class Todo(Base):
+    __tablename__ = "todos"
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String(100), nullable=False)
+    description = Column(String, nullable=True)
+    completed = Column(Boolean, default=False)
+    due_date = Column(Date, nullable=True)
---
+++ app/schemas.py
@@ -0,0 +1,20 @@
+from pydantic import BaseModel
+
+'''
+VALIDATION
+'''
+# Pydantic
+class TodoBase(BaseModel):
+    title: str
+    description: str| None = None
+    completed: bool = False
+
+class TodoCreate(TodoBase):
+    pass
+
+class TodoResponse(TodoBase):
+    id: int
+
+    class Config:
+        from_attributes = True
+
